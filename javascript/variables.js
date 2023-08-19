44ab0c769464894db04e06831792feb35a4473a1
vtcavin <vtcavin@gmail.com>
2023-08-19 13:04:04 +0530

    Added javascript variables

---
+++ javascript/variables.js
@@ -0,0 +1,31 @@
+// let x=5;
+//console.log('x value before',x)
+
+//var x=7;
+//console.log('x value before',x);
+
+//var x=8;
+//console.log('x value after',x);
+
+
+//let y=7;
+//console.log('x value before',y);
+
+//y=8;
+//console.log('x value after',y);
+
+//var redeclare  yes    reassign yes
+//let redeclare  no     reassign yes
+//const redeclare no    reassign no
+
+
+//const z=10;
+//console.log('z value before',z);
+
+ //z=11;
+//console.log('z value after',z);
+
+
+
+
+
