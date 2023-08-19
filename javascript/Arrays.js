3f37f3cac082727e571b4e5f09c793edcb3b83df
vtcavin <vtcavin@gmail.com>
2023-08-19 14:38:52 +0530

    Added Javascript Arrays

---
+++ javascript/Arrays.js
@@ -0,0 +1,10 @@
+const marks=[25,30,65,52,94,32,64,57,59,45,28,14,39,48,58,52,48,49,35,64,46,27,68]
+console.log(marks);
+console.log(marks[1]);
+console.log(marks[9]);
+console.log(marks[5]);
+console.log(marks[4]);
+console.log(marks[6]);
+
+
+//index from starting zero is applied in order to retrieve particular marks
