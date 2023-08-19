e249be44ac55c9a6af2d981ea91491a974505be2
vtcavin <vtcavin@gmail.com>
2023-08-19 14:25:46 +0530

    Added Javascript datatypes

---
+++ javascript/datatypes.js
@@ -0,0 +1,26 @@
+let x=2;
+console.log(typeof(x));
+
+let y=3.0
+console.log(typeof(x));
+
+let z='English';
+console.log(typeof(z));
+
+let a='true'
+console.log(typeof(x));
+
+let marks=[1,2,3,4,5];
+console.log(typeof(marks));
+
+console.log(marks[0]);
+console.log(marks[1]);
+console.log(marks[2]);
+console.log(marks[3]);
+console.log(marks[4]);
+
+let p="Hello";
+g="world";
+console.log(p+g);
+
+
