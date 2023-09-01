bcc32829cebcef09fd13280912f847a40284484b
vtcavin <vtcavin@gmail.com>
2023-09-01 22:16:29 +0530

    Adding Basic operators javascript

---
+++ javascript/operaors_practice.js
@@ -0,0 +1,31 @@
+var a=10;
+var b=20;
+console.log(a+b);
+
+var a=25;
+var b="Javascript";
+console.log(a+b);
+
+a=4;
+ b=4;
+console.log(a+b);
+
+a=4;
+ b=5;
+console.log(a+b);
+
+a="javascript";
+b=9;
+c=12;
+console.log(a+b+c);
+
+a=12;
+b=3;
+console.log(a*b);
+ var a=12;
+ var b="cavin";
+ console.log(a);
+console.log(a++);
+
+
+
