fce1443ccd51713077c0a38b55ec3a80d8eec87f
vtcavin <vtcavin@gmail.com>
2023-09-02 17:58:07 +0530

    Added Comparison_logical_opeartor

---
+++ javascript/Comparison_logical_opeartor.js
@@ -0,0 +1,13 @@
+// var a=5;
+// var b=2;
+// console.log(a+ "==" + b + ":");console.log(a>b);
+
+let a=5;
+b=10;
+console.log(a+ "!=" +b + ":");
+console.log(a!=b);
+
+const a=5;
+b=10;
+console.log(a+ "!=" +b + ":");
+console.log(a!=b);
