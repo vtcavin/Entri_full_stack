e302c12f549c9d6ae2e042641058bf7ef889559a
vtcavin <vtcavin@gmail.com>
2023-09-02 18:34:58 +0530

    Added Conditional_statements

---
+++ javascript/Conditional_statement.js
@@ -0,0 +1,37 @@
+// Four types of conditional statements
+// 1)if
+// 2)if-else
+// 3)if else if else 
+// 4)Switch
+
+
+let a=10;
+let b=12;
+if(a>b){
+
+
+console.log("Greater");
+}
+
+else{
+    console.log("Lesser");
+}
+
+ a=20;
+ b=30;
+if(b>a){
+    console.log("Bigger");
+}
+else{
+    console.log("Smaller");
+}
+
+
+var amount=1000;
+
+if(amount>2000){
+    console.log("Donate money");
+}
+else{
+    console.log("Dont donate");
+}
