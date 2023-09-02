603067b4b833074eab9e67af631b7c3c8b4c93f4
vtcavin <vtcavin@gmail.com>
2023-09-02 23:42:57 +0530

    Added Javascript Loops

---
+++ javascript/Loops_javascript.js
@@ -0,0 +1,14 @@
+// Types of loops:-
+// 1)for loop
+// 2)while loop
+// 3)do-while-loop
+// 4)for-in-loop
+// 5)for-of-loop
+
+
+var sum=0;
+for (let i=20;i<=30;i++){
+    sum=sum+i
+}
+console.log(sum);
+
