10b0b0af42f50df3a3085230974eadf95135b15e
vtcavin <vtcavin@gmail.com>
2023-09-02 22:50:53 +0530

    Added if_ifelse_else Javascript

---
+++ javascript/Conditional_if_if_else.js
@@ -0,0 +1,20 @@
+var score=prompt("Enter marks");
+var grade;
+if(score>=90){
+    grade="A";
+}
+else if(score>=80){
+    grade="B";
+}
+else if(score>=70){
+    grade="C";
+}
+else if(score>=60){
+    grade="D";
+}
+else{
+    grade="F";
+}
+console.count("your marks: "+ score+ "Grade: "+grade)
+
+
