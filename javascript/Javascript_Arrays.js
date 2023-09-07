8d76a18890b782e8b1599b22ebb1bfad46c7e92a
vtcavin <vtcavin@gmail.com>
2023-09-07 23:28:56 +0530

    Adding Javascript_Arrays

---
+++ javascript/Javascript_Arrays.js
@@ -0,0 +1,18 @@
+ var cricketers=["Sachin","Dhoni","Kohili"];
+ console.log(cricketers);
+
+ const singers=new Array('sonunigam','Arijit','Kumar sanu');
+console.log(singers);
+ singers[3]="Udit narayan"
+ console.log(singers[0]);
+ console.log(singers[1]);
+ console.log(singers[6]);
+
+
+var singer=["Sonu","Shaan","kk"];
+console.log(singer);
+
+var arr=[23,24,25,26,27,28];
+console.log(arr);
+//  Modifying is not accepting in arrays arr[6]=45;
+console.log(singer.length);
