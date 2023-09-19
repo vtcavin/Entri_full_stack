05ac2a29306c78ae48d8aa633686910840c99988
vtcavin <vtcavin@gmail.com>
2023-09-19 21:57:33 +0530

    Added cavin exam

---
+++ Elements_html/advanced_elements/basic.js
@@ -0,0 +1,7 @@
+const number=prompt("Enter a number");
+if(n%2==0){
+    console.log("Number is Even");
+}
+else{
+    console.log("Number is Odd");
+}
---
+++ javascript/cavin.js
@@ -0,0 +1,10 @@
+
+let numbers=[1,2,3,4,5,6,7,8,9,10];
+for(let i=0;i<10;i++)
+    if(numbers[i]%2==0){
+        console.log("Even Number");
+    }
+    else{
+        console.log("Odd Number");
+    }
+
