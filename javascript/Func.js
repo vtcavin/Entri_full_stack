270070a13f9b19532e6bf5ec10f2bd43008e6627
vtcavin <vtcavin@gmail.com>
2023-09-27 09:12:46 +0530

    Added Function Example1

---
+++ javascript/Func.js
@@ -0,0 +1,35 @@
+function sum(n1,n2){
+    var result=n1+n2;
+    return result;
+}
+console.log(sum(5,6));
+
+
+
+function multiply(num1,num2){
+    var answer=num1*num2;
+    return answer;
+}
+console.log("Sum of 5 and 4 is: " + multiply(5,4));
+console.log(multiply(5,4));
+
+
+function average(num1,num2,num3,num4){
+    var result= num1+num2+num3+num4%2;
+    console.log(result);
+   
+}
+average(1,2,3,4);
+
+
+
+
+
+
+
+
+
+
+
+
+
