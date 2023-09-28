a5e90d577c714ec53e0eb872709b44ef5e71ad4f
vtcavin <vtcavin@gmail.com>
2023-09-28 21:52:44 +0530

    Added console methods Javascript

---
+++ javascript/console_methods.js
@@ -0,0 +1,13 @@
+s="cavin"
+console.log("My name is "+ s);
+
+// Providing information to user
+console.log("My name" + s);
+
+// Warning message
+s= "cavin"
+console.warn("my", + s);
+
+console.error("");
+
+console.table
