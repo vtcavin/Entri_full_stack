280595f40e03bc9660fea0f9d252a4e0988be57b
vtcavin <vtcavin@gmail.com>
2023-10-06 08:20:39 +0530

    Adding Children

---
+++ javascript/Children.js
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accessing children</title>
+</head>
+<body>
+  <h3>
+    Lets welcome
+  </h3>
+  <p>Iam Cavin</p>
+  <script>
+    console.log(document.firstChild());
+    console.log(document.lastChild());
+  
+  </script>
+</body>
+</html>
